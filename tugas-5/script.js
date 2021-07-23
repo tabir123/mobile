e617db5124095cc2c9436e455f807decff6f4db0
tabir123 <tabiraf123@gmail.com>
2021-07-23 09:16:35 +0700

    mengumpulkan tugas5

---
+++ tugas-5/script.js
@@ -0,0 +1,8 @@
+var tabir = prompt("nama mahasiswa");
+var nilai = prompt("masukan nilai mu");
+ 
+if(nilai > 70){
+    document.write(tabir +'<br>selamat anda lulus dengan nilai :' + nilai)
+}else{
+    document.write('maaf anda tidak lulus')
+}
